55b4ef820b8e22775b7be0d5bdd2b2d80b7f887b
doncato <don.cato.dc11@gmail.com>
2022-11-22 20:56:07 +0100

    More frontend

--- web/rsc/scr.js
+++ web/rsc/scr.js
@@ -74,7 +74,7 @@ function writeFiles(data, loc) {
             last_char = first_char
             jumper = `<span class='marker' id='${first_char}'></span>`
         }
-        str += "<li><button onclick='fileButtonHandler(this.id)' " + `id='file-${e}'>` + e + "</button>" + jumper + "</li>"
+        str += "<li><button class='entry' onclick='fileButtonHandler(this.id)' " + `id='file-${e}'>` + e + "</button>" + jumper + "</li>"
     }
     str += "</ul>"
     document.getElementById("datalist").innerHTML = str;
